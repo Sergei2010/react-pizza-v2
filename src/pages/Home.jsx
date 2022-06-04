d784a3c136b02176e92e40088009b412171335c2
sergei2010 <sergejmatvejcuk323@gmail.com>
2022-06-04 21:39:05 +0200

    create redux fot category & sort

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// useSelector == useContext
+// useDispatch -> ВЫПОЛНИТЬ
 
+import { setCategoryId } from '../redux/slices/filterSlice';
 import Sort from '../components/Sort';
 import Categories from '../components/Categories';
 import PizzaBlock from '../components/PizzaBlock';
@@ -8,21 +12,29 @@ import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
 
 const Home = () => {
+	const dispatch = useDispatch();
+	const { categoryId, sort } = useSelector((state) => state.filter);
+	// const sortType = useSelector((state) => state.filter.sort.sortProperty);
+
 	const { searchValue } = React.useContext(SearchContext);
 	const [items, setItems] = React.useState([]);
 	const [currentPage, setCurrentPage] = React.useState(1)
 	const [isLoading, setIsLoading] = React.useState(true);
-	const [categoryId, setCategoryId] = React.useState(0);
-	const [sortType, setSortType] = React.useState(
+	// const [categoryId, setCategoryId] = React.useState(0);
+	/* const [sortType, setSortType] = React.useState(
 		{ name: 'популярности', sortProperty: 'raiting' }
-	);
+	); */
+	const onChangeCategory = (id) => {
+		//console.log('id', id)
+		dispatch(setCategoryId(id))
+	}
 
 	React.useEffect(() => {
 		setIsLoading(true);
 
 		const category = categoryId > 0 ? `category=${categoryId}` : '';
-		const sortBy = sortType.sortProperty.replace('-', '');
-		const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+		const sortBy = sort.sortProperty.replace('-', '');
+		const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
 		const search = searchValue ? `&search=${searchValue}` : '';
 
 		fetch(`https://629219decd0c91932b6d45bd.mockapi.io/items?
@@ -34,7 +46,7 @@ const Home = () => {
 				setIsLoading(false)
 			});
 		window.scrollTo(0, 0);
-	}, [categoryId, sortType, searchValue, currentPage])
+	}, [categoryId, sort.sortProperty, searchValue, currentPage])
 	const pizzas = items.map((item) => <PizzaBlock key={ item.id } { ...item } />);
 	const skeletons = [...new Array(6)].map((_, i) => (<Sceleton key={ i } />));
 
@@ -43,12 +55,9 @@ const Home = () => {
 			<div className="content__top">
 				<Categories
 					value={ categoryId }
-					onChangeCategory={ (i) => setCategoryId(i) }
+					onChangeCategory={ onChangeCategory }
 				/>
-				<Sort
-					value={ sortType }
-					onChangeSort={ (i) => setSortType(i) }
-				/>
+				<Sort />
 			</div>
 			<h2 className="content__title">Все пиццы</h2>
 			<div className="content__items">
